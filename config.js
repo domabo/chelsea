538cee432081ced8b154a9062adc22c68584adff
Guy Barnard <guy@domabo.com>
2017-08-08 06:20:09 -0500

    initial commit:

---
+++ config.js
@@ -0,0 +1,29 @@
+var config = {};
+
+//Debug
+config.debug = true;
+
+// MQTT Message Broker 
+config.mqtt = {
+    uri: 'ws://127.0.0.1:8883/',
+    options: {
+        //username: 'AnUser',
+        //password: 'APassword',
+        keepalive: 20,
+        clean: true,
+        clientId: 'chelsea-zwave',
+        rejectUnauthorized: false
+    }
+};
+
+// https://github.com/OpenZWave/open-zwave/wiki/Config-Options
+config.zwave = {
+    Logging: false, //Enable Logging in the Library or not.
+    EnableSIS: true, //Automatically become a SUC if there is No SUC on the network
+    ConsoleOutput: true, //Enable log output to stdout (or console)
+    SaveConfiguration: true, //When Shutting Down, should the library automatically save the Network Configuration in zwcfg_.xml_
+}
+
+config.root = 'iopa';
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,50 +1,27 @@
-var express = require('express');
-var app = express();
+const config = require('./config'),
+    platformid = "urn:io.resin:" + (process.env.RESIN_DEVICE_UUID || "localhost"),
+    locationid = process.env.RESIN_LOCATION_ID || "home",
+    mqtt = require('./lib/mqtt')({ locationid: locationid, platformid: platformid, config: config }),
+    zwave = require('./lib/zwave')({ locationid: locationid, platformid: platformid, config: config, eventSink: mqtt.publish });
+    mqtt.onmessage(zwave.invoke);
 
-// reply to request with "Hello World!"
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+var os = require('os');
 
-const PORT=8080;
-//start a server on port 80 and log its start to our console
-var server = app.listen(PORT, function () {
-
-  var port = server.address().port;
-  console.log('Example app listening on port ', port);
-
-});
-
-const DEVICE="/dev/cu.usbmodem1431";
-//const DEVICE="/dev/ttyACM0";
-
-var Zwave = require('./lib/openzwave.js');
-
-function main(){
-	var zwave = new Zwave();
-	zwave.open(DEVICE,false,function(){
-	var encender =false;
-	/*setInterval(function(){
-		if(encender){
-			zwave.swichOn(2);
-		} else {
-			zwave.swichOff(2);
-		}
-		encender=!encender;
-	},500);*/
-
-		zwave.swichOff(2);
-		/*zwave.swichOff(2);
-		zwave.swichOn(2);
-		zwave.swichOff(2);
-		zwave.swichOn(2);
-		zwave.swichOff(2);
-		zwave.swichOn(2);
-		zwave.swichOff(2);
-		zwave.swichOn(2);
-		zwave.swichOff(2);
-		zwave.swichOn(2);
-*/
-});
+zwavedriverpaths = {
+    "darwin": '/dev/cu.usbmodem1431',
+    "linux": '/dev/ttyACM0',
+    "windows": '\\\\.\\COM3'
 }
-// main();
+
+process.removeAllListeners('SIGINT');
+
+process.on('SIGINT', function () {
+    console.log('disconnecting...');
+    zwave.disconnect();
+
+    mqtt.end(function () {
+        process.exit();
+    });
+});
+
+zwave.connect(zwavedriverpaths[os.platform()]);
